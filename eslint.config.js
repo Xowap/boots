42a1a5ebec414b3caf4384c25f81edfbd9fda15a
Rémy Sanchez <remy.sanchez@hyperthese.net>
2026-04-22 14:45:27 +0200

    WIP: save state before lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
 import ts from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import importPlugin from 'eslint-plugin-import';
 import security from 'eslint-plugin-security';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
